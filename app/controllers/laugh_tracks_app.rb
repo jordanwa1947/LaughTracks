dc2af0038637186f0fd5b6d96cb5db320b5d9bed
Jordan Whitten <jordanwa1947@gmail.com>
2018-10-06 10:57:52 -0600

    finished total specials and test

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -3,8 +3,14 @@ class LaughTracksApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/comedians' do
-    @comedians = Comedian.all
-    @specials = Special.all 
+    if params[:age] == nil
+      @comedians = Comedian.all
+    else
+      @comedians = Comedian.all.find_all do |comedian|
+        comedian.age == params[:age].to_i
+      end
+    end
+    @specials = Special.all
     erb :comedians
   end
 end
